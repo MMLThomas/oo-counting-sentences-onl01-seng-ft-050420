0386143520fc7890a7b0a24454a496f7ccc23770
michael thomas <michael.thomas2014@gmail.com>
2020-05-14 11:47:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,8 +19,8 @@ class String
 
   def count_sentences
     sentences = 0
-    for(i=0; i < self.length; i += 1)
-      if self[i] == "." || "!" || "?"
+    for i in self
+      if i == "." || "!" || "?"
         sentences += 1
       end
     end
